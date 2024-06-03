38ea3b5c74c8c814fb72302326d1d7deb94a6e2b
Sahykas <147314136+Sahykas@users.noreply.github.com>
2024-06-03 15:46:48 +0300

    Add files via upload

---
+++ measurement/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+
+
+class Measurement(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements')
+    temperature = models.FloatField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
